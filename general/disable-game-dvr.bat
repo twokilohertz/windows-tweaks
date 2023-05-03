1a490c42b1028c5f0862f3347de5ee3476610247
Adam Macdonald <72780006+twokilohertz@users.noreply.github.com>
2023-05-03 14:46:57 +0100

    Add additional registry entry to disable Game Capture

--- general/disable-game-dvr.bat
+++ general/disable-game-dvr.bat
@@ -1,2 +1,3 @@
+reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\GameDVR" /v AllowGameDVR /t REG_DWORD /d 0 /f
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" /v AppCaptureEnabled /t REG_DWORD /d 0 /f
 reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v GameDVR_Enabled /t REG_DWORD /d 0 /f
