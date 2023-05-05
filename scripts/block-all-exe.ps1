d026172d111235cf58d67493c211cf7718283cc6
Adam Macdonald <72780006+twokilohertz@users.noreply.github.com>
2023-05-05 02:14:41 +0100

    Minor capitalisation fix

--- scripts/block-all-exe.ps1
+++ scripts/block-all-exe.ps1
@@ -3,6 +3,6 @@ $executables = Get-ChildItem -Path "." -Recurse -Filter "*.exe"
 foreach ($exe in $executables) {
 	$exeName = $exe.Name
 	$exePath = $exe.FullName
-	New-NetFirewallRule -DisplayName "Blocked inbound: $exeName" -Description "$exePath" -Program "$exePath" -Direction Inbound -Action Block -Enabled True
-	New-NetFirewallRule -DisplayName "Blocked outbound: $exeName" -Description "$exePath" -Program "$exePath" -Direction Outbound -Action Block -Enabled True
+	New-NetFirewallRule -DisplayName "Blocked Inbound: $exeName" -Description "$exePath" -Program "$exePath" -Direction Inbound -Action Block -Enabled True
+	New-NetFirewallRule -DisplayName "Blocked Outbound: $exeName" -Description "$exePath" -Program "$exePath" -Direction Outbound -Action Block -Enabled True
 }
