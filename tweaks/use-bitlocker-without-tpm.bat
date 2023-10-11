00f0da045b5bb3b338cce271555137d881d24328
Adam Macdonald <72780006+twokilohertz@users.noreply.github.com>
2023-10-12 00:45:41 +0100

    Update use-bitlocker-without-tpm.bat

--- tweaks/use-bitlocker-without-tpm.bat
+++ tweaks/use-bitlocker-without-tpm.bat
@@ -1,2 +1,3 @@
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\FVE" /v UseAdvancedStartup /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\FVE" /v EnableBDEWithNoTPM /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\FVE" /v UseTPM /t REG_DWORD /d 2 /f
