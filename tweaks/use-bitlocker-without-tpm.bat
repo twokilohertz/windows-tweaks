965674caa31131512018c36d263e86d66ad5cf98
Adam Macdonald <72780006+twokilohertz@users.noreply.github.com>
2023-10-11 21:51:52 +0100

    Add to tweak to use BitLocker without a TPM

---
+++ tweaks/use-bitlocker-without-tpm.bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\FVE" /v UseAdvancedStartup /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\FVE" /v UseTPM /t REG_DWORD /d 2 /f
